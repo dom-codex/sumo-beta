df7e541f784cb2004ff194e84ff70a8b67d2d136
dom-codex <dominicwest7@gmail.com>
2020-05-24 10:49:13 +0100

    fix pagination bug and ui update bug

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const adminrouter = require("./routes/adminRoutes");
 const errorController = require('./controllers/erros')
 //models
 const User = require("./models/user");
-const uri = process.env.db ||`mongodb+srv://dominic:compressor@sumo-nd9pi.gcp.mongodb.net/test?retryWrites=true&w=majority` ;
+const uri = process.env.db ;
 
 //template engine configuration
 app.set("view engine", "ejs");
@@ -39,11 +39,11 @@ next()
 //session store initialization
 app.use(
   session({
-    secret: process.env.session_signing || 'chemicalengineering',
+    secret: process.env.session_signing,
     resave: false,
     saveUninitialized: false,
     store:new MongoStore({ 
-      url: process.env.session_store || `mongodb+srv://dominic:compressor@sumo-nd9pi.gcp.mongodb.net/test?retryWrites=true&w=majority`}) , 
+      url: process.env.session_store }) , 
       cookie:{
         maxAge:60*60*1000*24*7 //session will last for a week
       }, 
