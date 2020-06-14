fcf76f42682dee243b5632868f37d5cec1700273
dom-codex <dominicwest7@gmail.com>
2020-06-14 22:21:12 +0100

    add fade in effect

--- public/effect.js
+++ public/effect.js
@@ -1,8 +1,4 @@
-const uid = document.querySelector('#uid');
-const chat = document.querySelector(".chatlist");
-const close = document.querySelector(".close");
-const link = document.querySelector("#link");
-
-close.addEventListener("click", () => {
-  document.querySelector(".modal").style.display = "none";
+const docBody = document.querySelector('body');
+document.addEventListener('DOMContentLoaded',()=>{
+  docBody.classList.add('showBody');
 });
